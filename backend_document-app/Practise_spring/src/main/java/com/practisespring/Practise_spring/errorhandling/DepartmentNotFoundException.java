25aa05d6ecc22f714958442068e3829f2c319115
Shilpa2504 <1js20cs151@gmail.com>
2026-04-07 00:36:02 +0530

    fix: remove fallback defaults from datasource cog

--- backend_document-app/Practise_spring/src/main/java/com/practisespring/Practise_spring/errorhandling/DepartmentNotFoundException.java
+++ backend_document-app/Practise_spring/src/main/java/com/practisespring/Practise_spring/errorhandling/DepartmentNotFoundException.java
@@ -6,7 +6,7 @@ public class DepartmentNotFoundException extends Exception{
     }
 
     public DepartmentNotFoundException(String message){
-        super(message);
+        super(messagge);
     }
 
 
