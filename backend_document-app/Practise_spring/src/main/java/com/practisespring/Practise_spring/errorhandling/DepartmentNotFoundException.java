6d9420eb9f09f859d8c86961507df685c9dd86a7
Shilpa2504 <1js20cs151@gmail.com>
2026-04-07 01:00:00 +0530

    fix: remove fallback defaults from daog

--- backend_document-app/Practise_spring/src/main/java/com/practisespring/Practise_spring/errorhandling/DepartmentNotFoundException.java
+++ backend_document-app/Practise_spring/src/main/java/com/practisespring/Practise_spring/errorhandling/DepartmentNotFoundException.java
@@ -6,7 +6,7 @@ public class DepartmentNotFoundException extends Exception{
     }
 
     public DepartmentNotFoundException(String message){
-        super(messagge);
+        super(message);
     }
 
 
